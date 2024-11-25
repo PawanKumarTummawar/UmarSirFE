f74d59f68bce8da019f8b942b6baa1aea32f6ffd
Pawankumar Tummawar <pawantummawar333@gmail.com>
2024-11-25 10:49:00 +0530

    added switch on/off and passed switch and clear buttons as props

--- ReactJS/calculator/src/components/calculator.jsx
+++ ReactJS/calculator/src/components/calculator.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 // import { evaluate } from 'mathjs';
 import './Calculator.css'; 
+import Button from './Button';
 
 function Calculator() {
   const [input, setInput] = useState('');
+  const [switch1, setSwitch1] = useState(false);
 
   // Function to handle button clicks
   const handleClick = (value) => {
@@ -14,25 +16,40 @@ function Calculator() {
       } catch {
         setInput('Error');
       }
-    } else if (value === 'C') {
-      setInput(''); // Clearing the input
     } else {
       setInput(input + value);
     }
-  };
+  };  
+
+  function handleSwitch1() {
+    setSwitch1(!switch1);
+  }
+  function handleClear()
+  {
+      setInput(''); // Clearing the input
+  } 
+
+
 
   return (
     <div className="calculator">
       <div className="display">{input || '0'}</div>  {/*if input exists display or 0 */}
+      
       <div className="buttons">
+      {/* <button className= "clear" onClick={() => setSwitch1(!switch1) }>
+          {switch1 ? "OFF" : "ON" }
+        </button> */}
+        <Button cssclass = "clear" onClickHandler = {handleSwitch1}> {switch1 ? "OFF" : "ON" } </Button>
         {['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '0', '.', '=', '+'].map((btn) => (
           <button key={btn} onClick={() => handleClick(btn)}>
             {btn}
-          </button>
+          </button> 
         ))}
-        <button className="clear" onClick={() => handleClick('C')}>
+        {/* <button className="clear" onClick={() => setInput('')}>
           C
-        </button>
+        </button> */}
+        <Button cssclass = "clear" onClickHandler = {handleClear}> C </Button>
+
       </div>
     </div>
   );
