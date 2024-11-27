c6647213711df862183f557de1054fefbab179dd
Pawankumar Tummawar <pawantummawar333@gmail.com>
2024-11-27 11:12:15 +0530

    useEffect and Landing Page creation

--- ReactJS/project3/src/App.jsx
+++ ReactJS/project3/src/App.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import LandingPage from './components/landingpage/LandingPage';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import UseEffectDemo from './components/useEffectDemo';
 
 const App = () => {
     return (
+        // <div>
+        //      {/* { <Header /> */}
+        //     <LandingPage />
+        //      {/* <Footer /> } */}
+        //         {/* // <UseEffectDemo /> */}
+        //  </div>
         <div>
-            <Header />
             <LandingPage />
-            <Footer />
-        </div>
-    );
+         </div>
+    );                
 };
 
 export default App;
---
+++ ReactJS/project3/src/components/UseEffectDemo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+// const use
+const UseEffectDemo = () => {
+    function incrementA() {
+        setA
+    }
+
+  return (
+    <div>
+        <button onClick={incrementA}>Increment A</button>
+        <button onClick={incrementB}>Increment B</button>
+        <button onClick={incrementC}>Increment C</button>
+    </div>
+  )
+}
+
+export default UseEffectDemo
