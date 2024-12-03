8fb444085313a419d506404f08ca5c75e201f138
Pawankumar Tummawar <pawantummawar333@gmail.com>
2024-12-03 12:11:28 +0530

    updation of apibackendfrontend

--- ReactJS/project-5-ApiCallingFromSpringBoot/src/components/RegisterProduct.jsx
+++ ReactJS/project-5-ApiCallingFromSpringBoot/src/components/RegisterProduct.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const RegisterProduct = () => {
+    const[userValues, setUserValues] = useState({});
+
+    function handleSubmit () 
+    {
+        alert("Form is Submitted Successfully")
+    }
     return (
         <div>
-            <form action="">
+            <form className='form-container' onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="prodTitle">Product Title</label>
                     <input
@@ -13,7 +19,43 @@ const RegisterProduct = () => {
                         value={userValues.rno}
                     />
                 </div>
-            </form>
+
+                <div>
+                    <label htmlFor="prodDescription">Product Description</label>
+                    <input
+                        type="text"
+                        name="productDescription"
+                        id="productDescription"
+                        value={userValues.Description}
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="prodTitle">Product Category</label>
+                    <input
+                        type="text"
+                        name="productCategory"
+                        id="productCategory"
+                        value={userValues.category}
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="photo">Upload Photo</label>
+                    <input
+                        type="file"
+                        name="photo"
+                        id="photo"
+                        value={userValues.image}
+                    />
+                </div>
+
+                <div className='button-group'>
+                    <button type='submit'>Register</button>
+                    <button type='button'>Cancel</button>    
+                </div> 
+            </form> 
+            {console.log(userValues)};
         </div>
     );
 };
